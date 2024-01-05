0bacd940431735bca18472c698d1299ed1bf2502
sebjustseb <sebjustseb@gmail.com>
2024-01-05 14:06:08 -0800

    hotel API

--- script.js
+++ script.js
@@ -29,7 +29,29 @@ function saveUserInput(originCity, originCode, destinationCity, destinationCode,
 	  localStorage.setItem('search', JSON.stringify(search));
 
 }
+async function getDestId(destination) {
+	var url = 'https://booking-com.p.rapidapi.com/v1/hotels/locations?locale=en-us&name=' + destination;
+	console.log("Hotel dest_id = ");
+	var options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '57dfbe13d6msh55f96b9ca680f6dp1ff0e6jsnbea2c6477590',
+			'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
+		}
+	};
 
+	try {
+		console.log('Hi Bryan');
+		var response = await fetch(url, options);
+		var result = await response.json();
+		console.log(result);
+		var destId = result[0].dest_id;
+		console.log('destID= '+ destId);
+		return destId;
+	} catch (error) {
+		console.error(error);
+	}
+}
 function onFormSubmit(event){
 	event.preventDefault();
 	var originInput = origin.value;
@@ -40,10 +62,14 @@ function onFormSubmit(event){
 
 	originCode = getAirportCode(origin.value);
 	destinationCode = getAirportCode(destination.value);
-
+	destId = getDestId(destinationInput); //hotel dest_Id (booking)
+	console.log('Hi Sebastian');
+	console.log(destinationInput);
 	console.log("Searching origin=" + originInput);
 	console.log("Searching destination=" + destinationInput);
 	getFlight(originCode, destinationCode);
+	console.log("Booking dest_ID for " + destinationInput);
+	getDestId(destinationInput);
 
 	//Save search data
 	saveUserInput(originInput, originCode, destinationInput, destinationCode, startDateInput, endDateInput, budgetInput);
@@ -71,7 +97,7 @@ const options = {
 }
 
 function getAirportCode(destination){
-var requestUrl = 'https://tripadvisor16.p.rapidapi.com/api/v1/flights/searchAirport?query='+ destination;
+var requestUrl = 'https://tripadvisor16.p.rapidapi.com/api/v1/flights/searchAirport?query=' + destination;
 
 console.log("Searching destination=" + destination);
 
@@ -96,4 +122,13 @@ try {
 }
 })();
 }
+
+
+//hotel function
+//get dest_id (booking)
+
+//use dest_id, dates to fetch hotel with lowest price
+// function getHotel() {
+
+// }
 searchBtn.addEventListener("click", onFormSubmit);
