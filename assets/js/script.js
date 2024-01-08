2794315ef30e7cab49b74895ca4ddbdeb4555366
sebjustseb <sebjustseb@gmail.com>
2024-01-08 14:03:56 -0800

    hotel api ctd

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,7 @@ function saveUserInput(originCity, originCode, destinationCity, destinationCode,
 	  localStorage.setItem('search', JSON.stringify(search));
 
 }
+
 async function getDestId(destination) {
 	var url = 'https://booking-com.p.rapidapi.com/v1/hotels/locations?locale=en-us&name=' + destination;
 	console.log("Hotel dest_id = ");
@@ -47,13 +48,73 @@ async function getDestId(destination) {
 		var response = await fetch(url, options);
 		var result = await response.json();
 		console.log(result);
-		var destId = result[0].dest_id;
-		console.log('destID= '+ destId);
-		return destId;
+		bookDestId = result[0].dest_id;
+		console.log('destID= '+ bookDestId);
+		bookHotelId = getHotelId();
+		return bookDestId;
 	} catch (error) {
 		console.error(error);
 	}
 }
+
+async function getHotelId(){
+	console.log(startDateEl.value, endDateEl.value);
+	var startDate = dayjs(startDateEl.value).format('YYYY-MM-DD');
+	var endDate = dayjs(endDateEl.value).format('YYYY-MM-DD');
+	console.log(bookDestId);
+var url = 'https://booking-com.p.rapidapi.com/v2/hotels/search?order_by=price&dest_id='+bookDestId+'&dest_type=city&checkin_date='+startDate+'&locale=en-gb&units=metric&filter_by_currency=USD&checkout_date='+endDate+'&adults_number=1&room_number=1&page_number=0&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&include_adjacency=true';
+console.log(url);
+var options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '57dfbe13d6msh55f96b9ca680f6dp1ff0e6jsnbea2c6477590',
+		'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
+	}
+};
+
+try {
+	var response = await fetch(url, options);
+	var result = await response.json();
+	console.log(result);
+	bookHotelId = result.results[0].id;
+	console.log('hotelId= '+ bookHotelId);
+	getHotelPrice();
+	return bookHotelId;
+} catch (error) {
+	console.error(error);
+}}
+
+async function getHotelPrice() {
+	console.log(startDate.value, endDate.value);
+	startDate = dayjs(startDate.value).format('YYYY-MM-DD');
+	endDate = dayjs(endDate.value).format('YYYY-MM-DD');
+	var startDate2 = dayjs(startDate.value);
+	var endDate2 = dayjs(endDate.value);
+	console.log(startDate2, endDate2, bookHotelId);
+	var url = 'https://booking-com.p.rapidapi.com/v2/hotels/calendar-pricing?currency_code=USD&locale=en-us&checkin_date='+ startDate +'&hotel_id='+ bookHotelId +'&adults_number=1&checkout_date='+ endDate;
+	console.log(url);
+	var duration = startDate2.diff(endDate2, 'day');
+	console.log(duration + ' days');
+	var options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '57dfbe13d6msh55f96b9ca680f6dp1ff0e6jsnbea2c6477590',
+			'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
+		}
+	};
+
+	try {
+		var response = await fetch(url, options);
+		var result = await response.json();
+		console.log(result);
+		bookHotelPrice = result.avDates[0];
+		console.log(bookHotelPrice);
+		return bookHotelPrice;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
 function onFormSubmit(event){
 	event.preventDefault();
 	var originInput = origin.value;
@@ -64,15 +125,15 @@ function onFormSubmit(event){
 
 	originCode = getAirportCode(origin.value);
 	destinationCode = getAirportCode(destination.value);
-	destId = getDestId(destinationInput); //hotel dest_Id (booking)
 	console.log('Hi Sebastian');
 	console.log(destinationInput);
 	console.log("Searching origin=" + originInput);
 	console.log("Searching destination=" + destinationInput);
 	getFlight(originCode, destinationCode);
 	console.log("Booking dest_ID for " + destinationInput);
-	getDestId(destinationInput);
-  
+	bookDestId = getDestId(destinationInput);
+	// bookHotelPrice = getHotelPrice();
+	console.log('USD ' + bookHotelPrice);
 	
 	//Save search data
 	saveUserInput(originInput, originCode, destinationInput, destinationCode, startDateInput, endDateInput, budgetInput);
